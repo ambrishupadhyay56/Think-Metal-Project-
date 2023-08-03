624cbc6a6807374733cd6061d510353a40685b04
ambrishupadhyay56 <141358771+ambrishupadhyay56@users.noreply.github.com>
2023-08-03 23:38:18 +0530

    Add files via upload

---
+++ NodeMCU__ESP8266_/NodeMCU__ESP8266_.ino
@@ -0,0 +1,60 @@
+#include "config.h"
+
+AdafruitIO_Feed *Fan_Speed = io.feed("Fan_Speed");
+AdafruitIO_Feed *Switch = io.feed("Switch");
+
+void setup() {
+
+  // start the serial connection
+  Serial.begin(115200);
+
+  // wait for serial monitor to open
+  while(! Serial);
+
+  Serial.print("Connecting to Adafruit IO");
+
+  // start MQTT connection to io.adafruit.com
+  io.connect();
+
+  // set up a message handler for the count feed.
+  // the handleMessage function (defined below)
+  // will be called whenever a message is
+  // received from adafruit io.
+  Fan_Speed->onMessage(handleMessage);
+  Switch->onMessage(handleMessage);
+
+  // wait for an MQTT connection
+  // NOTE: when blending the HTTP and MQTT API, always use the mqttStatus
+  // method to check on MQTT connection status specifically
+   while(io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  // Because Adafruit IO doesn't support the MQTT retain flag, we can use the
+  // get() function to ask IO to resend the last value for this feed to just
+  // this MQTT client after the io client is connected.
+  Fan_Speed->get();
+  Switch->get();
+
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
+
+}
+
+void loop() {
+
+  // io.run(); is required for all sketches.
+  // it should always be present at the top of your loop
+  // function. it keeps the client connected to
+  // io.adafruit.com, and processes any incoming data.
+  io.run();
+}
+
+void handleMessage(AdafruitIO_Data *data) {
+
+ // Serial.print("received <- ");
+  Serial.println(data->value());
+
+}
